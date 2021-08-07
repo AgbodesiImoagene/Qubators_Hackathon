89825f94ec29d120596bc3b7cefa95b68276f361
Desi <agbodesii@gmail.com>
2021-08-07 19:37:04 +0100

    Update userVariables.py

--- userVariables.py
+++ userVariables.py
@@ -1,8 +1,8 @@
 # Set your preferences here
 
-# Enter API keys
-BINANCE_API_KEY = 'xTzCnBVFhz9METg1R15KcYHYueFhD8LJLu6BCgvUDEmB5mZWlOSMZd6rUklVaVh3'
-BINANCE_SECRET_KEY = 'JJkaL1sE5vNQgS2xQgAJ2Ibkx2Khgm40zZTCYdmMukqQz6Ovl9MoHhARPozLn7hm'
+# Enter valid API keys
+BINANCE_API_KEY = ''
+BINANCE_SECRET_KEY = ''
 use_testnet = True # Set to True use a testnet else False
 # Set testnet keys to '0' if you dont wish to use one
 TESTNET_API_KEY = 'Roleg41iCQnHmlRxETn5klezzns7gyv9Bc2HC9jmxXI5ukM0jaoqIhplGC6uRsyq'
@@ -10,7 +10,7 @@ TESTNET_SECRET_KEY = 'MGPf0jDxH6QSgu2V41j8qHZ1fZL7Xa478Uwjh5mWvhhSAivceJnZG4F14a
 
 trade_allocation = 0.2 # Fraction of capital to enter into each trade
 quote = 'USDT' # Quote ticker
-base = 'ETH' # Base ticker
+base = 'BTC' # Base ticker
 close_time = '18:30:00' # Enter the desired closing time in the form 'HH:MM:SS' in GMT
 tp = 0.02 # Enter the take-profit limit as a fraction of lot size
 sl = 0.01 # Enter the stop-loss limit as a fraction of lot size
